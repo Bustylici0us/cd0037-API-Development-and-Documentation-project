8230362afe938db8adfc8db2c62ce452a347121b
Bustylici0us <161844907+Bustylici0us@users.noreply.github.com>
2024-07-04 10:08:55 +0200

    Update test_flaskr.py

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -13,7 +13,8 @@ class TriviaTestCase(unittest.TestCase):
     def setUp(self):
         """Define test variables and initialize app."""
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        database_user_passwd = "postgres:postgres"
+        self.database_path = "postgresql://{}@{}/{}".format(database_user_passwd, 'localhost:5432', self.database_name)
         
         self.app = create_app({
             "SQLALCHEMY_DATABASE_URI": self.database_path
@@ -26,10 +27,67 @@ class TriviaTestCase(unittest.TestCase):
         """Executed after reach test"""
         pass
 
-    """
-    TODO
-    Write at least one test for each test for successful operation and for expected errors.
-    """
+    def test_get_categories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['categories'])
+
+    def test_get_questions(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['questions'])
+        self.assertTrue(data['total_questions'])
+
+
+    def test_delete_question(self):
+        res = self.client().delete('/questions/1')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        
+    
+    def test_delete_question_not_found(self):
+        res = self.client().delete('/questions/1000')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Page Not Found')
+    
+    def test_create_question(self):
+        total_questions_before = len(Question.query.all())
+        res = self.client().post('/questions', json={
+            'question': 'This is a test question',
+            'answer': 'This is a test answer',
+            'category': 3,
+            'difficulty': 2
+        })
+    
+        data = json.loads(res.data)
+        total_questions_after = len(Question.query.all())
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(total_questions_after, total_questions_before + 1)
+    
+    def test_if_question_create_fails(self):
+        new_question = {
+        'question': 'This is a Testquestion without additional fields',
+        }
+        response = self.client().post('/questions', json=new_question)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 422)
+        self.assertFalse(data['success'])
+        self.assertEqual(data['message'], 'Unprocessable Entity')
+
 
 
 # Make the tests conveniently executable
